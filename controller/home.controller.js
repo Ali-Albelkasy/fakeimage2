ad0064279595a4c04849f65b21eb49cded02eadb
Ali-Albelkasy <albelkasy697901@gmail.com>
2023-11-13 16:54:29 +0200

    sdsd

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -7,7 +7,7 @@ module.exports.render = (req, res) => {
 module.exports.handle = async (req, res) => {
     const { img } = req.body
     console.log(img);
-    const data = await imgModel.insertMany({ img:'https://fake-image-486da5bae1c5.herokuapp.com/'+img })
+    const data = await imgModel.insertMany({ img:'https://fake-image2-01daa7239862.herokuapp.com/'+img })
     console.log(data);
     res.redirect('/home')
 }
