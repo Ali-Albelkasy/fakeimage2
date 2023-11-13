9bd4cea01129fe01d0113d0723750ae866e37f9a
Ali-Albelkasy <albelkasy697901@gmail.com>
2023-11-13 16:49:59 +0200

    fgdg

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const upload = multer({ dest: '/images', storage })
 app.use(upload.single('img'))
 app.use(require('./router/app.routes'))
 app.get('/', (req, res) => res.send('Hello World!'))
-mongoose.connect('mongodb://127.0.0.1/hooks').then(() => {
+mongoose.connect('mongodb+srv://Albelkasy:niDzXFwsDmBnmh8N@cluster0.5tl86zq.mongodb.net/?retryWrites=true&w=majority').then(() => {
     console.log('connected');
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
